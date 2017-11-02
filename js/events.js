4355381408b5e6d80f9ed30a0dae1f2acd5e8897
Stavroula Guerrero <4stacyg@optonline.net>
2017-11-02 03:00:19 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,9 +11,15 @@ function frameIt() {
 }
 
 function pressIt() {
-$("input").on("keydown", function(e) {
+  $("input").on("keydown", function(e) {
    if (e.which === 71) {
       alert("G was pressed.")
     }
   })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert ("Your form is going to be submitted now.")
+  }
 }
