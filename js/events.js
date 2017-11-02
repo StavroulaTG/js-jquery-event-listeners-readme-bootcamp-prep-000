723eae0c0f7ef7ab159b833b92bdc2523e268c9f
Stavroula Guerrero <4stacyg@optonline.net>
2017-11-02 02:39:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,3 +9,10 @@ function frameIt() {
     $("img").addClass("tasty")
   })
 }
+
+function pressIt() {
+$(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+})
